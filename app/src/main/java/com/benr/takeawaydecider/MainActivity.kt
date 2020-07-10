6b66a8d396132379d67af8f5c3f05b4c2a893056
Ben Ross <benjamin.ross@youview.com>
2020-07-10 16:55:45 +0100

    When apps starts, display a random takeaway place

--- app/src/main/java/com/benr/takeawaydecider/MainActivity.kt
+++ app/src/main/java/com/benr/takeawaydecider/MainActivity.kt
@@ -2,10 +2,17 @@ package com.benr.takeawaydecider
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val places = arrayOf("Amici Miei", "Atari-Ya", "Iro Sushi", "La Lluna, Eat Tokyo")
+
+        val randomPlace = places.random()
+
+        selection.text = randomPlace
     }
 }
